f002697586a01cd4a7aede54bb8f0bf1ea7939d5
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-11-14 00:02:25 +0100

    Add files via upload

--- pandore.py
+++ pandore.py
@@ -1,39 +1,65 @@
 from os import name, chdir
 from os.path import isfile
+from PIL import Image
 from pystyle import Anime, Colorate, Colors, Center, System, Write
 
-code = """# Scarecrow
-
-# https://github.com/billythegoat356/Scarecrow
-
-
-from psutil import process_iter, NoSuchProcess, AccessDenied, ZombieProcess
-
-class scare:
-
-    def fuck(names):
-        for proc in process_iter():
-            try:
-                for name in names:
-                    if name.lower() in proc.name().lower():
-                        proc.kill()
-            except (NoSuchProcess, AccessDenied, ZombieProcess):
-                pass
-
-    def crow():
-        forbidden = ['http', 'traffic', 'wireshark']
-        return scare.fuck(names=forbidden)
-    
-scare.crow()
-
-
-# by billythegoat356
-\n\n\n"""
 
 
 if name == 'nt':
     path = '/'.join(__file__.split('\\')[:-1])
     chdir(path)
+
+
+
+class build:
+  def __init__(self, imagepath: str, scale: int) -> None:
+    self.path = imagepath
+    self.scale = scale
+
+    return self.run()
+
+  def run(self) -> None:
+    img = self.mkimage(path=self.path)
+    pixels = self.mkpixels(img=img)
+    ascii = self.mkascii(pixels=pixels)
+
+    with open(self.npath, mode="w", encoding='utf-8') as f:
+      f.write(ascii)
+    
+    return None
+
+  def mkimage(self, path: str) -> object:
+    img = Image.open(path)
+    width, height = img.size
+
+    self.nwidth = round(width / self.scale)
+
+    img = img.resize((self.nwidth, round(height / (self.scale * 2))))
+
+    return img.convert('L')
+
+  def mkpixels(self, img: object) -> str:
+    pixels = img.getdata()
+
+    pixels = [self.chars[pixel//25] for pixel in pixels]
+    return ''.join(pixels).replace('.',' ')
+
+  def mkascii(self, pixels: str) -> str:
+    ascii = [pixels[index:index + self.nwidth] for index in range(0, len(pixels), self.nwidth)]
+    nascii = []
+    for line in ascii:
+      if line.strip():
+        nascii.append(line)
+    return "\n".join(nascii)
+
+  @property
+  def npath(self) -> str:
+    return ".".join(self.path.split(".")[:-1]) + ".txt"
+
+  @property
+  def chars(self) -> str:
+    return ["§","/","#","&","@","$","%","*","!",":","."]
+
 
 
 ascii = """
@@ -46,12 +72,10 @@ ascii = """
  |   |    |  _( )_  |  (_,_)\  |  (_.\.' / \ `'/. \  ) /|  | \ `'   /\  `-'    / 
  /   )    \ (_ A _) |  |    |  |       .'   '. \_/``'.' |  |  \    /  \       /  
  `---'     '.(_,_).''--'    '--'-----'`       '-----'   ''-'   `'-'    `'-..-' 
-"""
+"""[1:]
 
 
 flower = '''
-This animation was made with *pystyle* :)
-
                 ..ooo.
              .888888888.
            .888"P""T"T888 8o
@@ -78,7 +102,7 @@ o88888"888"88o.  "8888"".88   .oo888oo..
                 o8P       "888"""
           ...oo88
  "8oo...oo888""
-'''
+'''[1:]
 
 
 
@@ -91,29 +115,32 @@ Anime.Fade(Center.Center(flower), Colors.red_to_green, Colorate.Vertical, interv
 
 
 def main():
-    System.Clear()
-    print("\n"*2)
-    print(Colorate.Diagonal(Colors.red_to_yellow, Center.XCenter(ascii)))
-    print("\n"*5)
+  System.Clear()
+  print("\n"*2)
+  print(Colorate.Diagonal(Colors.red_to_green, Center.XCenter(ascii)))
+  print("\n"*5)
+  
+  file = Write.Input("Drag an image -> ", Colors.green_to_yellow, interval=0.005)
+  
+  if not isfile(file):
+      print()
+      print(Colorate.Error("Error! This file does not exist!"))
+      return
 
-
-    file = Write.Input("Drag a Python file -> ", Colors.red_to_yellow, interval=0.005)
+  scale = Write.Input("Enter the scale (recommended: 8) -> ", Colors.green_to_yellow, interval=0.005)
+  
+  try:
+    scale = int(scale)
+  except:
     print()
+    print(Colorate.Error("Error! The scale has to be an integer"))
+    return
 
-    if not isfile(file):
-        print(Colorate.Error("Error! This file does not exist!"))
-        return
-
-    with open(file, mode='r', encoding='utf-8') as f:
-        content = f.read()
-
-    content = code + content
-
-    with open("scarecrowed.py", mode='w', encoding='utf-8') as f:
-        f.write(content)
-
-    Write.Input("Done!", Colors.red_to_yellow, interval=0.005)
-    return exit()
+  build(imagepath=file, scale=scale)
+  
+  print()
+  Write.Input("Done!", Colors.green_to_yellow, interval=0.005)
+  return exit()
         
 
 if __name__ == '__main__':
